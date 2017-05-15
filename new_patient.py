e73c3163da4d149ffc57c3a30e89135e7c0e01a0
rkartje <rkartje@aol.com>
2017-05-15 23:48:57 +0000

    patient class update

--- new_patient.py
+++ new_patient.py
@@ -1,14 +1,21 @@
 class Patient:
-    def __init__(self, name, birthdate, medical_number, phone_number):
+    #init is a function that tacks some info onto an object
+    #extend patient class to include diagnosis, medications, provider_id
+    
+    def __init__(self, name, birthdate, medical_number, phone_number, \
+    diagnosis, medications, provider_id):
         self.name = name
         self.birthdate = birthdate
         self.medical_number = medical_number
         self.phone_number = phone_number
-    
-    def record_encounter(self, date, reading):
+        self.diagnosis = diagnosis
+        self.medications = medications
+        self.provider_id = provider_id
+        
+        def record_encounter(self, date, reading):
         # to do
         pass
-    
+
     #methods - mutators, to assign values to fields
     def set_name (self, name):
         self.__name = name #assigns value in name parameter to a Patient's object's __name field
@@ -22,8 +29,17 @@ class Patient:
     def set_phone_number (self, phone_number):
         self.__phone_number = phone_number
         
+    def set_diagnosis (self, diagnosis):
+        self.__diagnosis = diagnosis
+        
+    def set_medications (self, medications):
+        self.__medications = medications
+        
+    def set._provider_id (self, provider_id):
+        self.__provider_id = provider_id
+        
     #methods - assessors, to get values from fields
-    def get_name(self):
+    def get_name (self):
         return self.__name
         
     def get_birthdate (self):
@@ -34,3 +50,13 @@ class Patient:
         
     def get_phone_number (self):
         return self.__phone_number
+        
+    def get_diagnosis (self):
+        return self.__diagnosis
+        
+    def get_medications (self):
+        return self.__medications
+        
+    def get_provider_id (self):
+        return self.__provider_id
+        
